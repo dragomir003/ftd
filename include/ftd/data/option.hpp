e6404239df6b19e256b081b0971f8973e224afed
dragomir003 <dragolekovic@gmail.com>
2022-12-17 19:15:10 +0100

    Added cmake and started work on optional

---
+++ include/ftd/data/option.hpp
@@ -0,0 +1,7 @@
+#ifndef FTD_DATA_OPTION_HPP
+#define FTD_DATA_OPTION_HPP
+
+template <typename T>
+struct Option{};
+
+#endif
---
+++ tests/src/option.cpp
@@ -0,0 +1,7 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include <ftd/data/option.hpp>
+
+TEST_CASE("Tests for option", "[option]") {
+    REQUIRE(1 == 1);
+}
